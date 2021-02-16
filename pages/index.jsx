bfc8851bcdf7b70c8c5bf39fa84d547092b7715b
Velo <hartley.max@outlook.com>
2021-02-16 18:22:51 +0000

    _app.js undeleted..

---
+++ pages/index.jsx
@@ -0,0 +1,17 @@
+import Head from 'next/head';
+import styles from '../styles/Home.module.css';
+
+export default function Home() {
+	return (
+		<>
+			<Head>
+				<title>Create Next App</title>
+				<link rel='icon' href='/favicon.ico' />
+			</Head>
+
+			<main>
+				<h1>Hello World</h1>
+			</main>
+		</>
+	);
+}
