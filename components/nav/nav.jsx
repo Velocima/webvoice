d7b66d8d7e40d78aa00549857f32fca7334c1b99
Velo <hartley.max@outlook.com>
2021-02-16 19:14:06 +0000

    hover effect added to nav links

--- components/nav/nav.jsx
+++ components/nav/nav.jsx
@@ -1,21 +1,34 @@
 import styles from './nav.module.css';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
+import { useState } from 'react';
 
 export default function Nav() {
+	const { pathname } = useRouter();
+	const [navHovered, setNavHovered] = useState(pathname);
+
 	const pages = [
 		{
 			page: 'Home',
 			path: '/',
 		},
 		{
-			page: 'Contact',
-			path: '/contact',
-		},
-		{
 			page: 'Portfolio',
 			path: '/portfolio',
 		},
+		{
+			page: 'Contact',
+			path: '/contact',
+		},
 	];
+
+	const handleMouseEnter = (path) => {
+		setNavHovered(path);
+		console.log(path, navHovered);
+	};
+	const handleMouseLeave = () => {
+		setNavHovered(pathname);
+	};
 	return (
 		<nav className={styles.nav}>
 			<section className={styles.logo}>
@@ -23,12 +36,28 @@ export default function Nav() {
 				<h2>WebVoice</h2>
 			</section>
 			<ul className={styles.links}>
-				{pages.map((page) => {
+				{pages.map(({ page, path }) => {
 					return (
-						<li>
-							<Link href={page.path}>
-								<a>{page.page}</a>
+						<li
+							key={path}
+							onMouseEnter={() => {
+								handleMouseEnter(path);
+							}}
+							onMouseLeave={handleMouseLeave}
+						>
+							<Link href={path}>
+								<a>{page}</a>
 							</Link>
+							<div
+								style={{
+									transform:
+										pathname === path
+											? 'translateX(0)'
+											: navHovered === path
+											? 'translateX(0)'
+											: 'translateX(-110%)',
+								}}
+							></div>
 						</li>
 					);
 				})}
